3865ac6231c8caa69122d1fc17ca3ebebbefe6b3
Goro-K <69romankizil69@gmail.com>
2022-11-15 13:17:40 +0100

    better structure

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -5,8 +5,10 @@ import useDarkMode from '../../hook/useDarkMode';
 function Button() {
   const [colorTheme, setTheme] = useDarkMode();
   return (
-    <div className="rounded-3xl border-solid border-black border-2 flex items-center">
-      {colorTheme === 'light' ? <FontAwesomeIcon onClick={() => setTheme(colorTheme)} icon={faMoon} className="text-2xl py-1 px-2" /> : <FontAwesomeIcon onClick={() => setTheme(colorTheme)} icon={faSun} className="text-2xl py-1 px-2" /> }
+    <div className="rounded-full border-solid border-black border-2 flex items-center dark:border-slate-200">
+      {colorTheme === 'light'
+        ? <FontAwesomeIcon onClick={() => setTheme(colorTheme)} icon={faMoon} className="text-2xl py-1 px-1.5 w-6 h-7" />
+        : <FontAwesomeIcon onClick={() => setTheme(colorTheme)} icon={faSun} className="text-2xl py-1 px-1.5 w-6 h-7" /> }
     </div>
   );
 }
