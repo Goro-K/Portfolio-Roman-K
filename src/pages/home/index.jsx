dbc3842ec47996139094f8ec25ab28d9e7c53fce
Goro-K <69romankizil69@gmail.com>
2022-11-15 13:19:48 +0100

    add gallery

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,11 @@
 import Header from '../../components/header/index';
+import Gallery from '../../components/gallery/gallery';
 
 function Home() {
   return (
     <div className="mx-auto transition duration-500">
       <Header />
-      <div className=" bg-gray-300 dark:bg-slate-600 transition duration-500">
-        <p className="text-black dark:text-white">Test</p>
-      </div>
+      <Gallery />
     </div>
   );
 }
