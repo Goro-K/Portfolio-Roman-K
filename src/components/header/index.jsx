7069c1b452b1f8c37ebb1d595d4a8e7896911fce
Goro-K <69romankizil69@gmail.com>
2022-11-16 11:48:02 +0100

    change some style

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,19 +5,19 @@ import Button from '../button/button';
 
 function Header() {
   return (
-    <header className="bg-opacity-40 bg-slate-200 dark:bg-slate-800 dark:text-slate-200 transition duration-500">
-      <div className="p-4 bg-gradient-to-b from-primary font-bold dark:from-gray-400 ">
+    <header className="bg-opacity-40 bg-slate-200 dark:bg-slate-800 dark:text-slate-200">
+      <div className="p-4 bg-gradient-to-b from-primary dark:from-gray-400 ">
         <nav className="flex items-center justify-between">
           <div className="flex items-center">
-            <h1 className="text-4xl mx-4">
+            <h1 className="text-4xl mx-4 font-bold">
               Rk
               <span className="text-blue-500 dark:text-red-500">.</span>
             </h1>
             <ul>
-              <NavLink className="mx-7" href="#projects">Projets</NavLink>
-              <NavLink className="mx-7" href="#competences">Compétence</NavLink>
-              <NavLink className="mx-7" href="#experiences">Expérience</NavLink>
-              <NavLink className="mx-7" href="#contact">Contact</NavLink>
+              <NavLink className="mx-7 visited:font-bold " to="/">Projets</NavLink>
+              <NavLink className="mx-7 visited:font-bold " to="/competences">Compétence</NavLink>
+              <NavLink className="mx-7 visited:font-bold " href="#experiences">Expérience</NavLink>
+              <NavLink className="mx-7 visited:font-bold " href="#contact">Contact</NavLink>
             </ul>
             <Button className="transition duration-300" />
           </div>
