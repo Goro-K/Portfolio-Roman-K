3a00fea0c876318d6e35352e06ca3e3c35fcedfb
Goro-K <69romankizil69@gmail.com>
2022-11-16 11:48:26 +0100

    add page competence

---
+++ src/pages/competence/index.jsx
@@ -0,0 +1,11 @@
+import Header from '../../components/header/index';
+
+function Competence() {
+  return (
+    <div className="mx-auto transition duration-500">
+      <Header />
+    </div>
+  );
+}
+
+export default Competence;
