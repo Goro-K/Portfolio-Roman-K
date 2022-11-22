d31e1684d4134c98eec5bdd5d27b1cb906813de6
Goro-K <69romankizil69@gmail.com>
2022-11-22 15:07:27 +0100

    change the sun button and reverse the condition

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,14 +1,15 @@
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import useDarkMode from '../../hook/useDarkMode';
+import Sun from './sun.png';
 
 function Button() {
   const [colorTheme, setTheme] = useDarkMode();
   return (
     <div className="rounded-full border-solid border-black border-2 flex items-center dark:border-slate-200">
       {colorTheme === 'light'
-        ? <FontAwesomeIcon onClick={() => setTheme(colorTheme)} icon={faMoon} className="text-2xl py-1 px-1.5 w-6 h-7" />
-        : <FontAwesomeIcon onClick={() => setTheme(colorTheme)} icon={faSun} className="text-2xl py-1 px-1.5 w-6 h-7" /> }
+        ? <img src={Sun} onClick={() => setTheme(colorTheme)} className="sun text-2xl py-1 px-1.5 h-9 text-slate-200" aria-hidden="true" alt="" />
+        : <FontAwesomeIcon onClick={() => setTheme(colorTheme)} icon={faMoon} className="text-2xl py-1 px-1.5 w-6 h-7" />}
     </div>
   );
 }
