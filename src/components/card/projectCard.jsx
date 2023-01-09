ca709b46f7a22c65ee65f8a6e0ec0c265bd60d27
Goro-K <69romankizil69@gmail.com>
2023-01-09 14:29:38 +0100

    some changes

--- src/components/card/projectCard.jsx
+++ src/components/card/projectCard.jsx
@@ -1,23 +1,33 @@
-import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from "react-router-dom";
+import useIntersectionObserver from "../../hook/useIntersectionObserver";
 
-function Card({ title, image }) {
+function Card({ data }) {
+  const onIntersect = (entry) => {
+    entry.target.classList.add("fade-in");
+  };
+
+  const cardRef = useIntersectionObserver(onIntersect);
   return (
     <div>
-      <img src={image} alt="" className="w-full aspect-square rounded-2xl" />
+      <div
+        ref={cardRef}
+        className="hover:animate-pulse text-center relative h-64 w-64 lg:h-96 lg:w-96 snap-start card"
+      >
+        <NavLink
+          to={`/projet/${data.id}`}
+          className="h-full w-64 lg:w-96 aspect-square block z-0"
+        >
+          <div>
+            <img
+              src={data.logo}
+              alt=""
+              className="w-full aspect-square rounded-2xl"
+            />
+          </div>
+        </NavLink>
+      </div>
     </div>
   );
 }
-
-Card.propTypes = {
-  title: PropTypes.string,
-  image: PropTypes.string,
-};
-
-Card.defaultProps = {
-  title: null,
-  image: null,
-};
 
 export default Card;
