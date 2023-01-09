94dad6971f26a3214a7140c9e5889dc0c07d57ad
Goro-K <69romankizil69@gmail.com>
2023-01-09 14:26:05 +0100

    add some pages and components

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import './index.css';
-import Home from './pages/home/index';
-import Competence from './pages/competence/index';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Style
+import "./index.css";
+
+// Pages and components
+import Home from "./pages/home/index";
+import Projet from "./pages/projet/index";
+import Header from "./components/header";
+import Error from "./pages/error/index";
+import Footer from "./components/footer/footer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/competences" element={<Competence />} />
+        <Route path="/projet/:id" element={<Projet />} />
+        <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
