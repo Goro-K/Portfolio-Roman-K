90f891e53078915fddcb3cdc19664afe0c7280e4
Goro-K <69romankizil69@gmail.com>
2023-01-09 14:26:49 +0100

    some change

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,4 +1,4 @@
-import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import useDarkMode from '../../hook/useDarkMode';
 import Sun from './sun.png';
@@ -7,9 +7,21 @@ function Button() {
   const [colorTheme, setTheme] = useDarkMode();
   return (
     <div className="rounded-full border-solid border-black border-2 flex items-center dark:border-slate-200">
-      {colorTheme === 'light'
-        ? <img src={Sun} onClick={() => setTheme(colorTheme)} className="sun text-2xl py-1 px-1.5 h-9 text-slate-200" aria-hidden="true" alt="" />
-        : <FontAwesomeIcon onClick={() => setTheme(colorTheme)} icon={faMoon} className="text-2xl py-1 px-1.5 w-6 h-7" />}
+      {colorTheme === 'light' ? (
+        <img
+          src={Sun}
+          onClick={() => setTheme(colorTheme)}
+          className="sun text-2xl py-1 px-1.5 h-9 text-slate-200"
+          aria-hidden="true"
+          alt=""
+        />
+      ) : (
+        <FontAwesomeIcon
+          onClick={() => setTheme(colorTheme)}
+          icon={faMoon}
+          className="text-2xl py-1 px-1.5 w-6 h-7"
+        />
+      )}
     </div>
   );
 }
