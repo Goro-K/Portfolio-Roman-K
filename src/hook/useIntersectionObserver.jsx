929b661cb5f89bc3c67ed19a8fda17ec8d8c89d9
Goro-K <69romankizil69@gmail.com>
2023-01-09 19:02:30 +0100

    add a new class fade-out

--- src/hook/useIntersectionObserver.jsx
+++ src/hook/useIntersectionObserver.jsx
@@ -1,14 +1,18 @@
 import { useEffect, useRef } from "react";
 
-function useIntersectionObserver(onIntersect, onBreak) {
+function useIntersectionObserver({ onIntersect }) {
   const ref = useRef(null);
-
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
+          entry.target.classList.remove("fade-out");
+          entry.target.classList.add("fade-in");
           onIntersect(entry);
           observer.unobserve(entry.target);
+        } else {
+          entry.target.classList.remove("fade-in");
+          entry.target.classList.add("fade-out");
         }
       });
     });
