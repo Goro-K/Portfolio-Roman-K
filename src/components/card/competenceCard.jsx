6b47e80a45f1774a80e18334c47f9fb8723e1774
Goro-K <69romankizil69@gmail.com>
2023-01-09 14:29:12 +0100

    some change

--- src/components/card/competenceCard.jsx
+++ src/components/card/competenceCard.jsx
@@ -1,32 +1,100 @@
-import PropTypes, { string } from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import { useState } from "react";
+import useIntersectionObserver from "../../hook/useIntersectionObserver";
 
-const technologies = [
-  'images/icon/logo-technologies/html-icon.png',
-  'images/icon/logo-technologies/css-icon.png',
-  'images/icon/logo-technologies/sass-icon.png',
-  'images/icon/logo-technologies/js-icon.png',
-  'images/icon/logo-technologies/tailwind-icon.png',
-  'images/icon/logo-technologies/nodejs-icon.png',
-  'images/icon/logo-technologies/express-icon.png',
-  'images/icon/logo-technologies/mongoDB-icon.png',
-  'images/icon/logo-technologies/react-icon.png',
-];
+function CompetenceCard({ data }) {
+  const [isClicked, setIsClicked] = useState(false);
 
-function CompetenceCard() {
+  function handleClick() {
+    setIsClicked(!isClicked);
+  }
+
+  const onIntersect = (entry) => {
+    entry.target.classList.add("fade-in");
+  };
+
+  const cardIntersect = useIntersectionObserver(onIntersect);
   return (
-    technologies.map((technologie, index) => (
+    <div>
       <div
-        key={`technologie ${index + 1}`}
-        className="text-center relative h-40 w-40 snap-start"
+        className={`${
+          isClicked
+            ? "animate-pulse text-center z-20 relative h-40 w-40 snap-start cursor-pointer"
+            : "hover:animate-pulse text-center z-20 relative h-40 w-40 snap-start cursor-pointer card"
+        }`}
+        onClick={handleClick}
+        ref={cardIntersect}
       >
-        <NavLink to="/competences" className="h-full w-full aspect-square block z-0">
-          <div>
-            <img src={technologie} alt="" className="w-full aspect-square" />
-          </div>
-        </NavLink>
+        <div className="h-40 w-40 aspect-square block">
+          <img src={data.image} alt="" className="w-full aspect-square" />
+        </div>
       </div>
-    ))
+      {isClicked ? (
+        <div className="fixed flex justify-center items-center flex-col bg-opacity-40 bg-gradient-to-b from-primary dark:from-gray-500 top-0 bottom-0 right-0 left-0 z-30 delay-300">
+          <div className="bg-slate-200 dark:bg-slate-900 w-72 h-auto pb-2 pr-1 z-50 rounded-lg dark:text-slate-200 sm:w-96">
+            <div className="flex items-center justify-between p-2">
+              <h2 className="text-4xl font-bold"> {data.name} </h2>
+              <div
+                className="float-right"
+                onClick={() => setIsClicked(!isClicked)}
+                role="button"
+                tabIndex={0}
+                aria-hidden="true"
+              >
+                <span
+                  className={
+                    isClicked
+                      ? "first:translate-y-1 first:-rotate-45 block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                      : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                  }
+                >
+                  {}
+                </span>
+                <span
+                  className={
+                    isClicked
+                      ? "hidden w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                      : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                  }
+                >
+                  {}
+                </span>
+                <span
+                  className={
+                    isClicked
+                      ? "last:-translate-y-1 last:rotate-45 block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                      : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                  }
+                >
+                  {}
+                </span>
+              </div>
+            </div>
+            <div className="ml-4 mb-4 flex justify-between items-center">
+              <div>
+                <p> Utilisé dans : </p>
+                <ul>
+                  {data.usedFor.map((used, index) => (
+                    <li key={`utilisé - ${index}`}>{used}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <img src={data.image} alt="" className="h-20 mr-4" />
+              </div>
+            </div>
+            <div className="ml-4">
+              <p>Connaissance de la technologie : </p>
+              <ul>
+                {data.knowledge.map((knowledge, index) => (
+                  <li key={`connaissance - ${index}`}>{knowledge}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
   );
 }
+
 export default CompetenceCard;
