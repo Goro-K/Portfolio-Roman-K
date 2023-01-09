f61a5ba3dc54d079dc6fa8ad04e5f6a17e85c06d
Goro-K <69romankizil69@gmail.com>
2023-01-09 14:29:25 +0100

    some changes

--- src/components/card/experienceCard.jsx
+++ src/components/card/experienceCard.jsx
@@ -1,38 +1,109 @@
-import PropTypes from 'prop-types';
-import useHover from '../../hook/useHover';
+import { useState } from "react";
+import useIntersectionObserver from "../../hook/useIntersectionObserver";
 
 const experiences = [
   {
-    image: 'images/icon/logo-experience/openclassrooms.png',
-    title: 'OpenClassrooms',
-    durée: 'Durée : 8 mois',
+    image: "images/icon/logo-experience/openclassrooms.webp",
+    title: "OpenClassrooms",
+    durée: "Durée : 8 mois",
   },
 ];
 
 function ExperienceCard() {
-  const [hoverRef, isHovered] = useHover();
+  const [isClicked, setIsClicked] = useState(false);
+
+  function handleClick() {
+    setIsClicked(!isClicked);
+  }
+
+  const onIntersect = (entry) => {
+    entry.target.classList.add("fade-in");
+  };
+
+  const cardRef = useIntersectionObserver(onIntersect);
   return (
-    experiences.map((experience, index) => (
-      <div key={`experience n°${index + 1}`} className="text-center relative h-60 w-72 snap-start" ref={hoverRef}>
-        <img src={experience.image} alt="" className="w-full h-full aspect-square relative rounded-2xl" />
-        {isHovered
-          ? (
-            <p className="absolute bottom-5 text-white w-full items-center text-xl font-bold ">
-              {experience.durée}
-            </p>
-          )
-          : null}
-      </div>
-    ))
+    <div>
+      {experiences.map((experience, index) => (
+        <div
+          key={`experience n°${index + 1}`}
+          className={`${
+            isClicked
+              ? "animate-pulse text-center relative h-56 w-56 lg:h-72 lg:w-72 snap-start cursor-pointer"
+              : "hover:animate-pulse text-center relative h-56 w-56 lg:h-72 lg:w-72 snap-start cursor-pointer card"
+          }`}
+          onClick={handleClick}
+          ref={cardRef}
+        >
+          <img
+            src={experience.image}
+            alt=""
+            className="w-full aspect-square rounded-2xl"
+          />
+        </div>
+      ))}
+      {isClicked ? (
+        <div className="fixed flex justify-center items-center flex-col bg-opacity-40 bg-gradient-to-b from-primary dark:from-gray-500 top-0 bottom-0 right-0 left-0 z-30">
+          <div className="bg-slate-200 dark:bg-slate-900 w-72 h-auto pb-2 pr-1 z-50 rounded-lg dark:text-slate-200 sm:w-96">
+            <div className="flex items-center justify-between p-2">
+              <h2 className="text-2xl font-bold"> OpenClassrooms </h2>
+              <div
+                className="float-right"
+                onClick={() => setIsClicked(!isClicked)}
+                role="button"
+                tabIndex={0}
+                aria-hidden="true"
+              >
+                <span
+                  className={
+                    isClicked
+                      ? "first:translate-y-1 first:-rotate-45 block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                      : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                  }
+                >
+                  {}
+                </span>
+                <span
+                  className={
+                    isClicked
+                      ? "hidden w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                      : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                  }
+                >
+                  {}
+                </span>
+                <span
+                  className={
+                    isClicked
+                      ? "last:-translate-y-1 last:rotate-45 block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                      : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+                  }
+                >
+                  {}
+                </span>
+              </div>
+            </div>
+            <div className="ml-4 mb-4 flex justify-between items-center">
+              <div>
+                <p>Durée : 8 mois</p>
+              </div>
+            </div>
+            <div className="ml-4">
+              <p className="mb-4">
+                L'étude se fait à distance et les différents projets se font
+                seuls, un suivi est établi par un mentor qui peut nous aider en
+                répondant à nos questions.
+              </p>
+              <p>
+                Durant ses 8 mois l'apprentissage des technologies comme HTML,
+                CSS, JS, NodeJS, se font à travers la pratique via différents
+                projets.
+              </p>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
   );
 }
-
-ExperienceCard.propTypes = {
-  image: PropTypes.string,
-};
-
-ExperienceCard.defaultProps = {
-  image: null,
-};
 
 export default ExperienceCard;
