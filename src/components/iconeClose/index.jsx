8192522d33b61b5059346f1f5dbde21a5d5fd42c
Goro-K <69romankizil69@gmail.com>
2023-01-09 19:02:03 +0100

    create component for icone for closing some element

---
+++ src/components/iconeClose/index.jsx
@@ -0,0 +1,41 @@
+function iconeClose({ setIsActive, isActive, hidden }) {
+  return (
+    <div
+      className={`md:${hidden} transition duration- ease-in-out`}
+      onClick={() => setIsActive(!isActive)}
+      role="button"
+      tabIndex={0}
+      aria-hidden="true"
+    >
+      <span
+        className={
+          isActive
+            ? "first:translate-y-1 first:-rotate-45 block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+            : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+        }
+      >
+        {}
+      </span>
+      <span
+        className={
+          isActive
+            ? "hidden w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+            : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+        }
+      >
+        {}
+      </span>
+      <span
+        className={
+          isActive
+            ? "last:-translate-y-1 last:rotate-45 block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+            : "block w-6 h-1 m-1 transition duration-300 ease-in-out bg-slate-900 dark:bg-slate-200 rounded-lg"
+        }
+      >
+        {}
+      </span>
+    </div>
+  );
+}
+
+export default iconeClose;
