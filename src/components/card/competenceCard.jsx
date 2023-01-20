e2a7e18d9c2aaf7282e9a2bca4e110032a62e32d
Goro-K <69romankizil69@gmail.com>
2023-01-20 07:55:25 +0100

    change the position of onClick

--- src/components/card/competenceCard.jsx
+++ src/components/card/competenceCard.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import useIntersectionObserver from "../../hook/useIntersectionObserver";
-import IconeClose from "../iconeClose/index";
-
 function CompetenceCard({ data }) {
   const [isClicked, setIsClicked] = useState(false);
 
@@ -30,13 +28,12 @@ function CompetenceCard({ data }) {
         </div>
       </div>
       {isClicked ? (
-        <div className="overlay">
-          <div className="bg-slate-200 dark:bg-slate-900 w-72 h-auto pb-2 pr-1 z-50 rounded-lg dark:text-slate-200 sm:w-96">
-            <div className="flex items-center justify-between p-2">
+        <div className="overlay" onClick={() => setIsClicked(!isClicked)}>
+          <div className="bg-slate-200 dark:bg-slate-900 w-72 h-auto mb-2 pr-4 z-50 rounded-lg dark:text-slate-200 sm:w-96">
+            <div className="flex items-center justify-between m-2">
               <h2 className="text-4xl font-bold"> {data.name} </h2>
-              <IconeClose setIsActive={setIsClicked} isActive={isClicked} />
             </div>
-            <div className="ml-4 mb-4 flex justify-between items-center">
+            <div className="m-4 flex justify-between items-center">
               <div>
                 <p> Utilisé dans : </p>
                 <ul>
@@ -46,11 +43,11 @@ function CompetenceCard({ data }) {
                 </ul>
               </div>
               <div>
-                <img src={data.image} alt="" className="h-20 mr-4" />
+                <img src={data.image} alt="" className="h-20" />
               </div>
             </div>
             <div className="ml-4">
-              <p>Connaissance de la technologie : </p>
+              <p className="text">Connaissance de la technologie : </p>
               <ul>
                 {data.knowledge.map((knowledge, index) => (
                   <li key={`connaissance - ${index}`}>{knowledge}</li>
