11396d38fa30f8d284f68a6b811ebf1528d0dcf4
Goro-K <69romankizil69@gmail.com>
2023-02-15 18:23:23 +0100

    change some icon

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,26 +1,19 @@
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 import useDarkMode from "../../hook/useDarkMode";
-import Sun from "./sun.png";
 
 function Button() {
   const [colorTheme, setTheme] = useDarkMode();
   return (
     <div className="rounded-full border-solid border-black border-2 flex items-center dark:border-slate-200">
       {colorTheme === "light" ? (
-        <img
-          src={Sun}
-          onClick={() => setTheme(colorTheme)}
-          className="sun text-2xl py-1 px-1.5 h-9 text-slate-200"
-          aria-hidden="true"
-          alt=""
-        />
+        <p onClick={() => setTheme(!colorTheme)}>
+          {" "}
+          <BsFillSunFill className="w-8 h-8 p-1" />{" "}
+        </p>
       ) : (
-        <FontAwesomeIcon
-          onClick={() => setTheme(colorTheme)}
-          icon={faMoon}
-          className="text-2xl py-1 px-1.5 w-6 h-7"
-        />
+        <p onClick={() => setTheme(colorTheme)}>
+          <BsFillMoonFill className="w-8 h-8 p-1" />
+        </p>
       )}
     </div>
   );
