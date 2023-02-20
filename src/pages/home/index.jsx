48083aa9def79068d0e9e9ccacae8a5133045aba
Goro-K <69romankizil69@gmail.com>
2023-02-20 18:18:15 +0100

    delete some text, add state

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,7 +5,7 @@ import ky from "ky";
 import Header from "../../components/header/index";
 
 function Home() {
-  // Variables d'état pour les données de projet
+  // State pour stocker les données de l'api
   const [projectData, setProjectData] = useState([]);
   const [technologieData, setTechnologieData] = useState([]);
   const [experienceData, setExperienceData] = useState([]);
@@ -36,52 +36,22 @@ function Home() {
 
   // système de recherche
   const [searchTerm, setSearchTerm] = useState("");
+  const [isWriting, setIsWriting] = useState(false);
 
   return (
     <>
-      <Header setSearchTerm={setSearchTerm} />
-      <div className="mx-auto bg-gray-200 dark:bg-slate-900 transition duration-500 py-5">
-        <div className="flex flex-col lg:flex-row text-4xl text-slate-900 dark:text-gray-300 transition duration-500 items-center lg:justify-evenly my-6 animate-appear">
-          <div className="text-center">
-            <h2 className="">Salut, Je suis Roman</h2>
-            <p className="m-0 text-3xl">Développeur Web</p>
-            <p className="text-3xl m-0 text-slate-900 dark:text-gray-100 font-bold">
-              basé sur Lyon
-            </p>
-          </div>
-          <div className="mt-3 w-5/6 lg:w-1/3">
-            <p className="text-base mb-2">
-              J&rsquo;ai rejoint en mars 2022 la formation Développeur Web de
-              chez OpenClassrooms pour en faire mon métier. Je maîtrise le HTML
-              & CSS et j&rsquo;essaie de maitriser le JavaScript à un niveau
-              satisfaisant.
-            </p>
-            <p className="text-base mb-2">
-              À l&rsquo;heure actuelle j&rsquo;ai travaillé sur 2 projets en JS
-              native (Kanap et HotTakes) et 2 projets avec React (Kasa et mon
-              PortFolio). J&rsquo;ai principalement travaillé sur des projets
-              Front, je n&rsquo;ai travaillé que sur un seul projet Back
-              (HotTakes), cependant je suis actuellement entrain de travailler
-              sur un projet Fullstack utilisant Express et MongoDB que je
-              connais déjà et React que j&rsquo;ai déjà partiellement compris.
-              <br />
-              Je suis ouvert à tout type de proposition et prêt à apprendre
-              d&rsquo;autres langages de programmation si vous êtes prêt à me
-              former/conseiller.
-            </p>
-            <p className="text-base">
-              Si vous souhaitez connaître plus de détails sur mes capacités, je
-              vous invite à cliquer sur les projets ci-dessous.
-            </p>
-          </div>
-          <div className="cursor-pointer">
-            <img
-              src={ProfilCV}
-              alt=""
-              className="mt-3 lg:mt-0 h-52 w-52 rounded-lg"
-              onClick={handleClick}
-            />
-          </div>
+      <Header setSearchTerm={setSearchTerm} setIsWriting={setIsWriting} />
+      {isWriting ? (
+        <div className="mx-auto bg-gray-200 dark:bg-slate-900 transition duration-500 py-5">
+          <Gallery
+            projectData={projectData}
+            technologieData={technologieData}
+            experienceData={experienceData}
+            searchTerm={searchTerm}
+          />
+        </div>
+      ) : (
+        <>
           <div className="text-base">
             {isClicked ? (
               <div className="overlay" onClick={() => setIsClicked(!isClicked)}>
@@ -98,15 +68,50 @@ function Home() {
               </div>
             ) : null}
           </div>
-        </div>
+          <div className="mx-auto bg-gray-200 dark:bg-slate-900 transition duration-500 py-5">
+            <div className="flex flex-col lg:flex-row text-4xl text-slate-900 dark:text-gray-300 transition duration-500 items-center lg:justify-evenly my-6 animate-appear">
+              <div className="text-center">
+                <h2 className="">Salut, Je suis Roman</h2>
+                <p className="m-0 text-3xl">Développeur Web</p>
+                <p className="text-3xl m-0 text-slate-900 dark:text-gray-100 font-bold">
+                  basé sur Lyon
+                </p>
+              </div>
+              <div className="mt-3 w-5/6 lg:w-1/3">
+                <p className="text-base mb-2">
+                  J&rsquo;ai rejoint en mars 2022 la formation Développeur Web
+                  de chez OpenClassrooms pour en faire mon métier. Je maîtrise
+                  le HTML, CSS et JavaScript.
+                </p>
+                <p className="text-base mb-2">
+                  Je suis ouvert à tout type de proposition et prêt à apprendre
+                  d&rsquo;autres langages de programmation si vous êtes prêt à
+                  me former/conseiller.
+                </p>
+                <p className="text-base">
+                  Si vous souhaitez connaître plus de détails sur mes capacités,
+                  je vous invite à cliquer sur les projets, technologies et experiences ci-dessous.
+                </p>
+              </div>
+              <div className="cursor-pointer">
+                <img
+                  src={ProfilCV}
+                  alt=""
+                  className="mt-3 lg:mt-0 h-52 w-52 rounded-lg"
+                  onClick={handleClick}
+                />
+              </div>
+            </div>
 
-        <Gallery
-          projectData={projectData}
-          technologieData={technologieData}
-          experienceData={experienceData}
-          searchTerm={searchTerm}
-        />
-      </div>
+            <Gallery
+              projectData={projectData}
+              technologieData={technologieData}
+              experienceData={experienceData}
+              searchTerm={searchTerm}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 }
