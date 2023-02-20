44b0ae40b8b3e3f3c565570a639f1d090eb723ca
Goro-K <69romankizil69@gmail.com>
2023-02-20 18:16:12 +0100

    change path error

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,6 @@ import "./index.css";
 // Pages and components
 import Home from "./pages/home/index";
 import Projet from "./pages/projet/index";
-
 import Error from "./pages/error/index";
 import Footer from "./components/footer/index";
 
@@ -18,9 +17,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/error" element={<Error />} />
         <Route path="/" element={<Home />} />
         <Route path="/projet/:id" element={<Projet />} />
-        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </BrowserRouter>
