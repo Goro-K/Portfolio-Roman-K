9644cb7e0e8ae6ce75d018436497cda6c73f5333
Goro-K <69romankizil69@gmail.com>
2023-02-20 18:17:05 +0100

    add urlParams

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
-function SearchBar({setSearchTerm, glassHeader, glassHome}) {
+function SearchBar({setSearchTerm, glassHeader, glassHome, setIsWriting}) {
+
+    // récupère la recherche dans la barre de recherche
+    const navigate = useNavigate();
 
     const [isClicked, setIsClicked] = useState(false)
 
     const handleSearchTerm = (e) => {
         let value = e.target.value
         setSearchTerm(value)
+        const urlSearchParams = new URLSearchParams();
+        urlSearchParams.append("term", value);
+        const newSearch = urlSearchParams.toString();
+        navigate({ search: newSearch, replace: true });
+        if (value.length === 0) {
+            setIsWriting(false)
+        } else {
+            setIsWriting(true)
+        }
     }
     return (
         <div className={`${glassHeader} ${glassHome}`}>
@@ -16,7 +29,7 @@ function SearchBar({setSearchTerm, glassHeader, glassHome}) {
             <BsSearch onClick={() => setIsClicked(!isClicked)} className="w-6 h-6 ml-2 mr-1"/> 
             </button>
             <input 
-                className={isClicked ? "p-1 mx-0.5 rounded-md focus:outline-none w-full" : "hidden"}
+                className={isClicked ? "text-slate-900 p-1 mx-0.5 rounded-md focus:outline-none w-full search-bar-open" : " py-1 mx-0.5 rounded-md search-bar-close"}
                 type="text" 
                 placeholder="Projets, Technos, Exp..."
                 onChange={handleSearchTerm}/>
