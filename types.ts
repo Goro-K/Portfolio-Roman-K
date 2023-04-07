202630b6fc5b4fc8e4adb28c80df597ee3420b6f
Goro-K <69romankizil69@gmail.com>
2023-04-07 16:54:31 +0200

    some config with vite.js (typecript)

---
+++ types.ts
@@ -0,0 +1,27 @@
+export interface Project {
+    id: string;
+    title: string;
+    logo: string;
+    description: string;
+    objectifs: Array<string>;
+    technologies: Array<string>;
+    lien: string;
+    imagesPc: Array<string>;
+    video: string;
+  }
+  
+  export interface Technologie {
+    id: string;
+    name: string;
+    image: string;
+    usedFor: Array<string>;
+    knowledge: Array<string>;
+  }
+  
+  export interface Experience {
+    image: string;
+    name: string;
+    durée: string;
+    méthode: string;
+    tech_learnt: string;
+  }
