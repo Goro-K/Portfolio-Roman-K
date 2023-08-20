9151f7ba1827ceda17b5aa8936c2ee440a068d80
Goro-K <69romankizil69@gmail.com>
2023-08-20 23:19:47 +0200

    change style

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -6,7 +6,7 @@ export default function Error() {
   return (
     <>
     <Header />
-    <div className="min-h-screen px-8 text-slate-900 dark:text-gray-300 bg-gray-200 dark:bg-slate-900 transition duration-500 py-5 text-center gap-10 flex flex-col">
+    <div className="min-h-screen px-8 text-lightHeadline dark:text-darkHeadline bg-lightGallery dark:bg-darkGallery transition duration-500 py-5 text-center gap-10 flex flex-col">
       <h1 className="text-9xl m-10 lg:m-16">404</h1>
       <h2 className="text-3xl mb-10 lg:text-5xl"> La page que vous recherchez est inexistante !</h2>
       <NavLink to="/" className="text-2xl mt-20 lg:text-4xl"> Retourner sur la page d'accueil </NavLink>
