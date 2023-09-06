d12edb20e28646bb59aef3064d714c95f86e1ccf
Goro-K <69romankizil69@gmail.com>
2023-09-06 09:02:13 +0200

    del getProject

--- src/api/getProject.ts
+++
@@ -1,7 +0,0 @@
-import { API_URL } from "../../config";
-import { Project } from "./../../types";
-
-export async function getProject(projectId: string): Promise<Project> {
-  const response = await fetch(`${API_URL}/projects/${projectId}`);
-  return response.json();
-}
